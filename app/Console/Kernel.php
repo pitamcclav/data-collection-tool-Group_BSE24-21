f65963cb05189eeaf2d79c6ee399a4c930b6767e
ssekitooleko.peter <pitamcclav@gmail.com>
2024-09-27 14:31:37 +0300

    hhhhh

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    
+
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
